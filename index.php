c1b3ff61861adc49f3d9c6e329d3e3a30db8d007
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-05-17 22:22:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,16 +9,47 @@ if(file_exists('core/Config.php')){
   die('Need to create config file!');
 }
 
-if(LoggedIn()){
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  Event('Logged In');
-  Event('Logged In - Page');
-  Error404();
+  <title>SmartMailer.io</title>
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+  
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.10.0/jquery.timepicker.min.js"></script>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.8/js/jquery.tablesorter.combined.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.8/css/theme.ice.min.css">
 
-}else{
+</head>
 
-  Event('Not Logged In');
-  Event('Not Logged In - Page');
-  Error404();
+<body>
 
-}
+  <nav class="navbar navbar-fixed-top navbar-dark bg-primary container">
+    <a class="navbar-brand" href="https://smartmailer.io">SmartMailer.io</a>
+    <ul class="nav navbar-nav">
+      
+    </ul>
+  </nav>
+
+  <div class="container">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <?php SmartMailer(); ?>
+        </div>
+      </div>
+  	</div>
+  </div><!-- /.container -->
+
+</body>
+</html>
